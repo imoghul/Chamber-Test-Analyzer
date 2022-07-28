d83a6555ec33ef77cc8f2c5a3bb55b3e90230ba0
immoghul <moghul02@gmail.com>
2022-07-28 15:39:27 -0400

    much more fancy processing

--- scripts/analysis_utils.py
+++ scripts/analysis_utils.py
@@ -4,6 +4,14 @@ import statistics
 from utils import *
 import statsmodels.api as sm
 import scipy
+import os
+from tqdm import tqdm
+
+progBars = True
+barTitle = "Processing"
+
+def getIterable(d):
+    return tqdm(d,desc=barTitle,leave=False) if progBars else d
 
 def dt(time, data):
     data = data.copy()
@@ -18,24 +26,25 @@ def dt(time, data):
 def getPeaks(t, data):
     res = []
     diff = dt(t, data)
-    for i, v in enumerate(diff):
+    for i, v in  getIterable(enumerate(diff)):
         if(v == 0 or (i != 0 and v*diff[i-1] <= 0)):
             res.append(i)
 
-    for i,v in enumerate(data[1:-1]):
-        if ((v > data[i-1] and v < data[i+1]) or (v < data[i-1] and v > data[i+1])) and i not in res:
-            res.append(i)
+    # for i,v in enumerate(data[1:-1]):
+    #     if ((v > data[i-1] and v < data[i+1]) or (v < data[i-1] and v > data[i+1])) and i not in res:
+    #         res.append(i)
     return res
 
-def getCleanPeaks(t,data,error=.1):
-    peaks = getPeaks(t,data)
+def getCleanPeaks(t,data,peaks,errorMax=.05):
+    peaks = peaks.copy()
     rm = []
-    for p in peaks:
+    for i,p in getIterable(enumerate(peaks)):
         try:
-            if(abs(data[p-1]-data[p])<=error and abs(data[p+1]-data[p])<=error):
+            if(abs(data[p-1]-data[p])<=errorMax and abs(data[p+1]-data[p])<=errorMax):
                 rm.append(p)
         except:
             pass
+    
     for i in rm:
         peaks.remove(i)
     return peaks
@@ -48,7 +57,7 @@ def smooth(arr, span):
     # The "my_average" part again. Slightly different to before, because the
     # moving average from cumsum is shorter than the input and needs to be padded
     front, back = [np.average(arr[:span])], []
-    for i in range(1, span):
+    for i in getIterable(range(1, span)):
         front.append(np.average(arr[:i + span]))
         back.insert(0, np.average(arr[-i - span:]))
     back.insert(0, np.average(arr[-2 * span:]))
@@ -85,17 +94,17 @@ def getSmooth(t, y, iterations=None, span=None):
         # if(len(y)):return len(y)*[average(y)]
         # else:return y
     smoov = smooth(y, span)
-    for i in range(iterations-1):
+    for i in getIterable(range(iterations-1)):
         smoov = smooth(smoov, span)
     return smoov
 
 
 
 
-def getNoiseChunks(t,y,margin=3): # returns [[a,b],[c,d],[e,f]...]
+def getNoiseChunks(t,y,margin=1.5): # returns [[a,b],[c,d],[e,f]...]
     chunks = []
     temp = []
-    for i,v in enumerate(y[:-1]):
+    for i,v in getIterable(enumerate(y[:-1])):
         if(abs(v-y[i+1])<=margin):
             if(temp==[]):temp = [i,i+1]
             else:
@@ -105,9 +114,9 @@ def getNoiseChunks(t,y,margin=3): # returns [[a,b],[c,d],[e,f]...]
             temp = []
     if(len(temp)>1):chunks.append(temp)
     return chunks
-def smoothNoiseChunks(t,y,margin = 3,iterations = None, span = None):
+def smoothNoiseChunks(t,y,margin = 1.5,iterations = None, span = None):
     y = y.copy()
-    for chunk in getNoiseChunks(t,y,margin):
+    for chunk in getIterable(getNoiseChunks(t,y,margin)):
         smoothed = getSmooth(t[chunk[0]:chunk[1]],y[chunk[0]:chunk[1]],iterations,span)
         y[chunk[0]:chunk[1]] = smoothed
     return y
--- scripts/test.py
+++ scripts/test.py
@@ -37,60 +37,52 @@ def analyze():
             t = interest["Test Time"]
             watts = interest["Watt"]
             wattHrs = watts.copy()
-            # for i, v in enumerate(wattHrs):
-            #     wattHrs[i] = (v*t[i])//3600
-            # watts = smoothNoiseChunks(t,watts,2,1,1)
-
-            origWatts = watts.copy()
-            watts = smoothNoiseChunks(t, watts)
-            dwattsdt = dt(t, watts)
-            wattsPeaks = getCleanPeaks(t, watts)
-            wattsPeaksDirty = getPeaks(t,watts)
-            writer.writerow(["Time (s): "]+t)
-            writer.writerow(["Original Watts: "] + [str(i) for i in origWatts])
-            writer.writerow(["Watts: "]+[str(i) for i in watts])
-            # writer.writerow(
-            #     ["Watts Peaks:"]+['1' if i in wattsPeaks else '' for i, _ in enumerate(watts)])
-            # writer.writerow(["dwatts/dt",""]+[str(i) for i in dt(t, watts)])
-            writer.writerow(["-"*len(t)])
 
 
-            plt.figure()
+            iterations = 4
+            subplots = [200+iterations*10+j+1 for j in range(2*iterations)]
+            for iter in getIterable(range(iterations)):
+                origWatts = watts.copy()
+                watts = smoothNoiseChunks(t, watts)
+                dwattsdt = dt(t, watts)
+                wattsPeaksDirty = getPeaks(t,watts)
+                wattsPeaks = getCleanPeaks(t, watts,wattsPeaksDirty)
+                # writer.writerow(["Time (s): "]+t)
+                # writer.writerow(["Original Watts: "] + [str(i) for i in origWatts])
+                # writer.writerow(["Watts: "]+[str(i) for i in watts])
+                # writer.writerow(
+                #     ["Watts Peaks:"]+['1' if i in wattsPeaks else '' for i, _ in enumerate(watts)])
+                # writer.writerow(["dwatts/dt",""]+[str(i) for i in dt(t, watts)])
+                # writer.writerow(["-"*len(t)])
 
-            # plt.subplot(211)
-            # plt.plot(t, origWatts, "paleturquoise")
-            # plt.plot(t, watts, "black")
-            # plt.plot([t[i] for i in wattsPeaks], [watts[i]
-            #          for i in wattsPeaks], "o")
-            # plt.title("watts")
+                plt.figure(fn)
+                w = plt.subplot(subplots[iter])#(211)
+                w.plot(t, interest["Watt"], "paleturquoise")
+                w.plot(t, origWatts, "turquoise")
+                w.plot(t, watts, "b")
+                # w.scatter(t, watts, c="blue")
+                for i in getNoiseChunks(t,origWatts):
+                    plt.plot( t[i[0]:i[1]] ,origWatts[i[0]:i[1]],"black")
+                w.scatter([t[i] for i in wattsPeaksDirty], [watts[i]
+                        for i in wattsPeaksDirty], c="red")
+                w.scatter([t[i] for i in wattsPeaks], [watts[i]
+                        for i in wattsPeaks], c="green")
+                plt.title("watts %d"%(iter+1))
+                
 
-
-            
-
-            # plt.subplot(212)
-            # plt.plot(t[1:], dwattsdt, "brown")
-            # plt.title("dwatts/dt")
-
-            # plt.show()
-
-            w = plt.subplot(211)
-            w.plot(t, interest["Watt"], "paleturquoise")
-            w.plot(t, origWatts, "turquoise")
-            w.plot(t, watts, "b")
-            for i in getNoiseChunks(t,origWatts):
-                plt.plot( t[i[0]:i[1]] ,origWatts[i[0]:i[1]],"black")
-            w.scatter([t[i] for i in wattsPeaksDirty], [watts[i]
-                     for i in wattsPeaksDirty], c="red")
-            w.scatter([t[i] for i in wattsPeaks], [watts[i]
-                     for i in wattsPeaks], c="green")
-
-            
-            plt.title("watts")
-            
-
-            dw = plt.subplot(212,sharex = w)
-            plt.title("dwatts/dt")
-            dw.plot(t[1:], dwattsdt, "o")
+                p = plt.subplot(subplots[iter+iterations],sharex = w)
+                plt.title("peaks %d"%(iter+1))
+                peaksT = [t[i] for i in wattsPeaksDirty]
+                peaksData = [watts[i]for i in wattsPeaksDirty]
+                p.plot(peaksT, peaksData,"red")
+                peaksData = smoothNoiseChunks(peaksT,peaksData)
+                p.plot(peaksT,peaksData)
+                peaksPeaksDirty = getPeaks(peaksT,peaksData)
+                peaksPeaks = getCleanPeaks(peaksT, peaksData,peaksPeaksDirty)
+                p.scatter([peaksT[i] for i in peaksPeaksDirty], [peaksData[i]
+                        for i in peaksPeaksDirty], c="red")
+                p.scatter([peaksT[i] for i in peaksPeaks], [peaksData[i]
+                        for i in peaksPeaks], c="green")
 
             plt.show()
 
@@ -101,19 +93,3 @@ def get_json(filename):
 
 
 analyze()
-# x = np.linspace(0,2*np.pi,100)
-# x+=x[-1]*10
-# y = np.sin(x) + np.random.random(100) * 0.8
-# y+=y[-1]*10
-# def smooth(y, box_pts):
-#     box = np.ones(box_pts)/box_pts
-#     y_smooth = np.convolve(y, box, mode='same')
-#     return y_smooth
-
-# plt.plot(x, y,'b')
-# plt.plot(x, smooth(y,3), 'orange', lw=2)
-# plt.plot(x, smooth(y,19), 'black', lw=2)
-# plt.plot(x[10:-10], smooth(y,3)[10:-10], 'r-', lw=2)
-# plt.plot(x[10:-10], smooth(y,19)[10:-10], 'g-', lw=2)
-
-# plt.show()
